2a0439031830af18d83190f3aad8bd6ab2c765ae
Victor Marques <vhm.marques@hotmail.com>
2021-08-02 22:04:13 -0300

    Criando a entidade Pessoa

---
+++ CadastroPessoas/Models/Pessoa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroPessoas.Models
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} Necessário")]
+        [StringLength(60, MinimumLength = 5, ErrorMessage = "{0} tamanho mínimo de 6 caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "{0} Necessário")]
+        [Display(Name = "Data de Nascimento")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime DtNascimento { get; set; }
+
+        public Pessoa()
+        {
+        }
+
+        public Pessoa(int id, string nome, DateTime dtNascimento)
+        {
+            Id = id;
+            Nome = nome;
+            DtNascimento = dtNascimento;
+        } 
+    }    
+}
