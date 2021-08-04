ca081e66af52e5c6d932e7eb3276f559d27d8863
Victor Marques <vhm.marques@hotmail.com>
2021-08-04 16:01:08 -0300

    Adicionada as ações de consultar, deletar e editar pessoas

--- CadastroPessoas/Controllers/PessoasController.cs
+++ CadastroPessoas/Controllers/PessoasController.cs
@@ -1,5 +1,6 @@
 ﻿using CadastroPessoas.Models;
 using CadastroPessoas.Services;
+using CadastroPessoas.Services.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -14,7 +15,7 @@ namespace CadastroPessoas.Controllers
 
         public PessoasController(PessoaService pessoaService)
         {
-            _pessoaService = pessoaService;        
+            _pessoaService = pessoaService;
         }
         public IActionResult Index()
         {
@@ -32,9 +33,88 @@ namespace CadastroPessoas.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Criar(Pessoa pessoa)
         {
-            _pessoaService.Insert(pessoa);
+            _pessoaService.Inserir(pessoa);
             return RedirectToAction(nameof(Index));
         }
 
+        public IActionResult Deletar(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _pessoaService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Deletar(int id)
+        {
+            _pessoaService.Remover(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        public IActionResult Detalhes(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _pessoaService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        public IActionResult Editar(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _pessoaService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Editar (int id, Pessoa pessoa)
+        {
+            if (id != pessoa.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _pessoaService.Atualizar(pessoa);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
+        }
     }
 }
--- CadastroPessoas/Services/PessoaService.cs
+++ CadastroPessoas/Services/PessoaService.cs
@@ -1,5 +1,7 @@
 ﻿using CadastroPessoas.Context;
 using CadastroPessoas.Models;
+using CadastroPessoas.Services.Exceptions;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,10 +23,40 @@ namespace CadastroPessoas.Services
             return _context.Pessoas.ToList();        
         }
 
-        public void Insert(Pessoa obj)
+        public void Inserir(Pessoa obj)
         {
             _context.Add(obj);
             _context.SaveChanges();
         }
+
+        public Pessoa FindById(int id)
+        {
+            return _context.Pessoas.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remover(int id)
+        {
+            var obj = _context.Pessoas.Find(id);
+            _context.Pessoas.Remove(obj);
+            _context.SaveChanges();
+        }
+
+        public void Atualizar(Pessoa obj)
+        {
+            if (!_context.Pessoas.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id não encontrado"); 
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+        }
+
     }
 }
